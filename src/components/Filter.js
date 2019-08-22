6f7b29797afffa723f8de2fa778b912754198124
Colin Nguyen <colinnguyen@Colins-MacBook-Pro.local>
2019-08-21 23:28:10 -0700

    continue of Filter logic

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,26 +3,46 @@ import {PropTypes} from 'prop-types';
 import Checkbox from '@material-ui/core/Checkbox';
 
 class Filter extends Component {
+    // constructor(props) {
+    //     super(props);
+    
+    //     this.state = {
+    //     id: ''
+    //     //   isChecked: false 
+    //     };
+    
+    //     // this.state={
+    //     //   isNameClicked: 'name',
+    //     //   isAgeClicked: 'age',
+    //     // }; 
+    //   }
 
-    changeHandler = (value, id) => {
-        if (value) {
-          this.props.onSelect(id);
-        } else {
-          // handle de-select
-        }
-      };
+    // changeHandler = (value, id) => {
+        
+    //     console.log(id)
+    //     console.log(value)
+    //     if (value) {
+    //       this.props.onSelect(id);
+    //       this.setState({id: id})
+    //     } else {
+    //       // handle de-select
+    //     }
+    //     // this.props.onSelect(id);
+    //     // this.setState({isChecked: value})
+    //   };
 
     render() {
-        const { rowData, selectedId, id } = this.props;
-        const isChecked = id === selectedId; 
+        // const isChecked
+        // const { selectedId } = this.props;
+        // const isChecked = this.state.id === selectedId; 
 
         return (
         <div className="checkboxes">
             <Checkbox 
                 // id={`checkbox_${id}`}
-                checked={isChecked}
+                // checked={isChecked}
                 // onChange={e => this.changeHandler(e.target.checked, id)}
-                onChange={e => this.props.onSelect('name')}
+                onChange={() => this.props.onSelect('name')}
 
                 // type="checkbox"
                 // name="record"
@@ -31,9 +51,10 @@ class Filter extends Component {
             />
             <label>Name</label>
             <Checkbox 
-                checked={isChecked}
+                // checked={isChecked}
+                onChange={() => this.props.onSelect('age')}
                 // onChange={e => this.changeHandler(e.target.checked, 'age')}
-                onChange={e => this.props.onSelect('age')}
+                // onChange={e => this.changeHandler(e.target.checked, 'age')}
                 // onChange={this.props.onAgeClick} 
                 // checked={this.props.isAgeChecked}
             />
